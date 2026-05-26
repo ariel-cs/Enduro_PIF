a323c22bcc2fc2b084731101c9157153f9122913
ariel-cs <aco4@cesar.school>
2026-05-26 11:00:41 -0300

    feat: create player.h

---
+++ include/player.h
@@ -0,0 +1,18 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+struct Player {
+    float z;
+    float x;
+    float speed;
+    float maxSpeed;
+    float acele;
+    float decele;
+    float friction;
+};
+
+void InitPlayer(struct Player *player);
+void UpdatePlayer(struct Player *player, float currentCurve);
+void DrawPlayer(void);
+
+#endif
